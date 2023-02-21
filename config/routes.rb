b39dd94f7d668c851c5f241ccfd377ca05085cb5
Marco Ricci <mr.ricci79@gmail.com>
2023-02-21 12:45:28 +0000

    task manager completed

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
   # root "articles#index"
   get '/tasks', to: 'tasks#index'
+  get '/tasks/new', to: 'tasks#new'
+  get '/tasks/:id', to: 'tasks#show', as: :task
+
+  post '/tasks', to: 'tasks#create'
+
+  get '/tasks/:id/edit', to: 'tasks#edit', as: :edit_task
+  patch 'tasks/:id', to: 'tasks#update'
+
+  delete '/tasks/:id', to: 'tasks#destroy'
 end
